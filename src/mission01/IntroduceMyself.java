520ad4bd7b2a8859a539bee88c8cce5982d38bef
nahye <nnhh.shine@gmail.com>
2024-10-30 17:58:37 +0900

    미션1완료

---
+++ src/mission01/IntroduceMyself.java
@@ -0,0 +1,11 @@
+package mission01;
+
+public class IntroduceMyself {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요!");
+        System.out.println("현재 자바를 공부하고있는 문나혜라고합니다.");
+        System.out.println("저는 매일 꾸준히 공부하는게 목표입니다.");
+        System.out.println("함께 좋은 영향받으면서, 같이 성장해나갔으면 좋겠어요!");
+        System.out.println("잘부탁드립니다~");
+    }
+}
